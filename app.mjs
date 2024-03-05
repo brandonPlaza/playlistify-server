42805c55e7821389e82f9b07df9d0bc23d8f8503
brandonPlaza <plazab@sheridancollege.ca>
2024-03-04 19:47:35 -0500

    Moved authentication call to server

--- app.mjs
+++ app.mjs
@@ -7,24 +7,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/login', function(req, res) {
+// app.get('/login', function(req, res) {
 
-  var state = (Math.random() * 10).toString(36).replace('.', '');
-  var scope = 'user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public user-library-read';
+//   var state = (Math.random() * 10).toString(36).replace('.', '');
+//   var scope = 'user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public user-library-read';
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
-});
-
-app.get('/callback', function(req, res){
-
-});
+//   res.redirect('https://accounts.spotify.com/authorize?' +
+//     querystring.stringify({
+//       response_type: 'code',
+//       client_id: client_id,
+//       scope: scope,
+//       redirect_uri: redirect_uri,
+//       state: state
+//     }));
+// });
 
 app.listen(port, () => {
   console.log(`Playlistify server listening on port: ${port}`);
